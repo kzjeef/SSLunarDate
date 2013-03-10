2ef92e2f0c3d6ee5aa3acbd176c3755802bc432b
Zhang Jiejing <kzjeef@gmail.com>
2013-03-10 22:07:25 +0800

    change copyright.

--- SSLunarDate/libLunar.h
+++ SSLunarDate/libLunar.h
@@ -3,7 +3,20 @@
 //  SSLunarDate
 //
 //  Created by Jiejing Zhang on 13-2-6.
-//  Copyright (c) 2013年 Jiejing Zhang. All rights reserved.
+//  Copyright (c) 2013 Jiejing Zhang. All rights reserved.
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software Foundation,
+//  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 //
 
 #ifndef SSLunarDate_libLunar_h
